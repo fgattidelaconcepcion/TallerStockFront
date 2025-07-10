db76ea8d841a040ed2d44b8231aa3495f4ef418e
fgatti <fgattidelaconcpecion@gmail.com>
2025-07-09 21:49:32 -0300

    first commit

---
+++ TallerStock/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerStock;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ TallerStock/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TallerStock;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
